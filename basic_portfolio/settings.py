b1ce1dc3767ee9050870da1534986802eb802d51
leatolu <markjtibbetts@gmail.com>
2017-09-03 22:32:27 +0200

    latest development snapshot

--- basic_portfolio/settings.py
+++ basic_portfolio/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'image_viewer.apps.ImageViewerConfig',
+    'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'basic_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- basic_portfolio/urls.py
+++ basic_portfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^image_viewer/', include('image_viewer.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^gallery/', include('gallery.urls')),
     url(r'^admin/', admin.site.urls),
 ]
